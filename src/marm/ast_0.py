78b5154d8172e9042b23a4b48d6f55f85ead846f
Marcial Gaißert <gaisseml@studi.informatik.uni-stuttgart.de>
2018-09-27 15:25:30 +0200

    Remove unneeded files (now merged to ast.py)

--- src/marm/ast_0.py
+++
@@ -1,9 +0,0 @@
-class Translationunit(Node):
-    def __init__(self,procdecl):
-        self.proc=procdecl
-class Procdecl(Node):
-    def __init__(self,returnType,name,params,body):
-        self.returnType = returnType
-        self.name = name
-        self.params = params
-        self.body = body
