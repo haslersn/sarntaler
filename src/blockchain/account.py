92eda2ae6fbb13088a38f707f5999f21e353aea2
Andreas Dolp <ga87loj@mytum.de>
2018-10-01 14:21:44 +0200

    labvm/blockchain: new storage implementation for account

--- src/blockchain/account.py
+++ src/blockchain/account.py
@@ -1,14 +1,37 @@
 import json
 from binascii import hexlify, unhexlify
-from typing import TypeVar, Generic
+from pydoc import locate
+from typing import List
+import logging
 
 from src.blockchain.crypto import compute_hash
 from collections import namedtuple
 
 
-T = TypeVar('T')  # maybe understand why this is needed
-class Storage(Generic[T]):
-    pass
+class StorageItem(namedtuple("StorageItem", ["s_name", "s_type", "s_value"])):
+    def __new__(cls, s_name: str, s_type: type, s_value: object):
+        return super().__new__(cls, s_name, s_type, s_value)
+
+    def to_json_compatible(self):
+        """ Returns a JSON-serializable representation of this object. """
+        val = {}
+        val["s_name"] = self.s_name
+        val["s_type"] = self.s_type.__name__
+        val["s_value"] = self.s_value
+        return val
+
+    @classmethod
+    def from_json_compatible(cls, val):
+        """ Create a new StorageItem from its JSON-serializable representation. """
+        s_type = locate(val['s_type'])  # gets type from serialized string
+        return cls(val["s_name"], s_type, val["s_value"])
+
+    def set_value(self, var_name: str, var_value: object):
+        if self.s_name != var_name or self.s_type != type(var_value):
+            logging.warning("can't set storage for variable " + var_name)
+            return None
+        return StorageItem(var_name, self.s_type, var_value)
+
 
 class Account(namedtuple("Account", ["pub_key", "balance", "code", "owner_access", "storage"])):
     _dict = dict()
@@ -20,7 +43,7 @@ class Account(namedtuple("Account", ["pub_key", "balance", "code", "owner_access
         else:
             return None
 
-    def __new__(cls, pub_key: bytes, balance: int, code: str, owner_access: bool, storage: list):
+    def __new__(cls, pub_key: bytes, balance: int, code: str, owner_access: bool, storage: List[StorageItem]):
         constructed = super().__new__(cls, pub_key, balance, code, owner_access, storage)
         if constructed.hash in cls._dict:
             return cls._dict[constructed.hash]
@@ -35,17 +58,22 @@ class Account(namedtuple("Account", ["pub_key", "balance", "code", "owner_access
         val["balance"] = self.balance
         val["code"] = self.code
         val["owner_access"] = self.owner_access
-        val["storage"] = self.storage
+        val["storage"] = []
+        for storage_item in self.storage:
+            val["storage"].append(storage_item.to_json_compatible())
         return val
 
     @classmethod
     def from_json_compatible(cls, val):
         """ Create a new account from its JSON-serializable representation. """
+        storage_items = []
+        for item in val["storage"]:
+            storage_items.append(StorageItem.from_json_compatible(item))
         return cls(unhexlify(val["pub_key"]),
                    val["balance"],
                    val["code"],
                    val["owner_access"],
-                   val["storage"])
+                   storage_items)
 
     @property
     def hash(self):
@@ -58,7 +86,36 @@ class Account(namedtuple("Account", ["pub_key", "balance", "code", "owner_access
         return compute_hash(self.pub_key)
 
     def add_to_balance(self, delta: int):
-        newbalance = self.balance + delta
-        if newbalance < 0:
-            raise ValueError("Invalid balance modification: New balance (" + str(newbalance) + ") would be negative")
+        new_balance = self.balance + delta
+        if new_balance < 0:
+            logging.warning("Invalid balance modification: New balance (" + str(new_balance) + ") would be negative")
+            return None
         return Account(self.pub_key, self.balance + delta, self.code, self.owner_access, self.storage)
+
+    def set_storage(self, var_name: str, var_value: object):
+        # you can set new values for existing variables with the correct type
+        find_item = list(
+            filter(lambda storage_item: storage_item.s_name == var_name and storage_item.s_type == type(var_value),
+                   self.storage))
+        if not find_item or len(find_item) != 1:
+            logging.warning("can't set storage for variable " + var_name)
+            return None
+        new_item = find_item[0].set_value(var_name, var_value)
+        new_storage = self.storage + [new_item]
+        new_storage.remove(find_item[0])
+        return Account(self.pub_key, self.balance, self.code, self.owner_access, new_storage)
+
+    def get_storage(self, var_name: str):
+        find_item = list(filter(lambda storage_item: storage_item.s_name == var_name, self.storage))
+        if not find_item or len(find_item) != 1:
+            logging.warning("can't get storage for variable " + var_name)
+            return None
+        return find_item[0].s_value
+
+#    def add_to_storage(self, storage_item: StorageItem):
+#        new_storage = self.storage + [storage_item]
+#        return Account(self.pub_key, self.balance, self.code, self.owner_access, new_storage)
+
+#    def remove_from_storage(self, var_name):
+#        new_storage = list(filter(lambda storage_item: storage_item.s_name != var_name, self.storage))
+#        return Account(self.pub_key, self.balance, self.code, self.owner_access, new_storage)
--- tests/test_account.py
+++ tests/test_account.py
@@ -1,20 +1,26 @@
 import json
 
-import pytest
+from src.blockchain.account import Account, StorageItem
 
-from src.blockchain.account import Account
 
-def test_tx_input():
-    account = Account(bytes(1), 0, "code", True, "test")
+def test_account_serializing():
+    storage = StorageItem("var", str, "value")
+    account = Account(bytes(1), 0, "code", True, [storage])
     jsonstr = json.dumps(account.to_json_compatible())
     print(jsonstr)
 
     restoredAccount = Account.from_json_compatible(json.loads(jsonstr))
-
     assert account.hash == restoredAccount.hash
     account2 = account.add_to_balance(5)
     assert account2.balance == account.balance + 5
     account3 = account2.add_to_balance(-5)
     assert account3.hash == account.hash
-    with pytest.raises(ValueError):
-        account3.add_to_balance(-1)
+    assert None == account3.add_to_balance(-1)
+
+
+def test_account_storage():
+    account_with_int = Account(bytes(1), 0, "code", True, [StorageItem("test_var", int, 42)])
+    assert None == account_with_int.set_storage("nonexistent", 3)
+    assert None == account_with_int.set_storage("test_var", "invalid")
+    new_account = account_with_int.set_storage("test_var", 27)
+    assert new_account.get_storage("test_var") == 27
