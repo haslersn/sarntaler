cc72650fe0c6e056312530b5c317d91623912633
haslersn <sebastian.hasler@gmx.net>
2018-10-01 18:44:38 +0200

    blockchain/state_transition: Code style

--- src/blockchain/state_transition.py
+++ src/blockchain/state_transition.py
@@ -1,52 +1,42 @@
 import typing
-import src.blockchain.new_transaction.py as t
+from src.blockchain.new_transaction.py import *
 from src.blockchain.merkle_trie import MerkleTrie
 from src.blockchain.account import Account
 from src.scriptinterpreter import ScriptInterpreter
 
-def transit(state : MerkleTrie, transaction : t.Transaction, miner_add : bytes):
-    transInputs =  transaction.tx_data.inputs
-    transOutputs = transaction.tx_data.outputs
+def transit(state : MerkleTrie, transaction : Transaction, miner_address : bytes):
+    tx_inputs =  transaction.tx_data.inputs
+    tx_outputs = transaction.tx_data.outputs
 
-
-    for transInput in transInputs:
-
-        if not state.contains(transInput.address):
-            #inputAcc not existing
+    for input in tx_inputs:
+        if not state.contains(input.address):
+            # input address does not exist
             return None
-
-        acc = Account.get_from_hash(state.get(transInput.address))
-
-        try:
-            acc = acc.add_to_balance(- transInput.value)
-        except ValueError:
-            # Transaction-Sender has not enough money
+        acc = Account.get_from_hash(state.get(input.address))
+        acc = acc.add_to_balance(- input.value)
+        if acc is None:
+            # couldn't spend value
             return None
+        state = state.put(input.address, acc.hash)
 
-        state = state.put(transInput.address, acc.hash)
-
-    for transOutput in transOutputs:
-        if not state.contains(transOutput.address):
-            #outputAcc not existing
+    for output in tx_outputs:
+        if not state.contains(output.address):
+            # output address does not exist
             return None
-
-        acc = acc.get_from_hash(state.get(transOutput.address))
-
-        acc = acc.add_to_balance(transOutput.value)
-        state = state.put(transOutput.address, acc.hash)
-
-        if acc.code != None:
-            vm = ScriptInterpreter(state, transOutput.params, acc, transaction.hash)
+        acc = Account.get_from_hash(state.get(output.address))
+        acc = acc.add_to_balance(output.value)
+        state = state.put(output.address, acc.hash)
+        if acc.code is not None:
+            vm = ScriptInterpreter(state, output.params, acc, transaction.hash)
             state = vm.execute_script()
 
-
-    #Miner gets his fee
-    if not state.contains(miner_add):
-        #no miner address
-        return None
-
-    acc = Account.get_from_hash(state.get(miner_add))
-    acc = acc.add_to_balance(transaction.tx_data.fee)
-    state = state.put(acc, acc.hash)
+    if miner_address != bytes(32):
+        # Miner gets his fee if the miner_address is not zero
+        if not state.contains(miner_address):
+            #no miner address
+            return None
+        acc = Account.get_from_hash(state.get(miner_address))
+        acc = acc.add_to_balance(transaction.tx_data.fee)
+        state = state.put(miner_address, acc.hash)
 
     return state
