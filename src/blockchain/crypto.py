52ec5f1def5981c31b4a0cfe394d8ea38f440638
haslersn <sebastian.hasler@gmx.net>
2018-10-02 10:01:25 +0200

    blockchain/crypto: new functionality for rsa keys

--- src/blockchain/crypto.py
+++ src/blockchain/crypto.py
@@ -1,4 +1,7 @@
 from hashlib import sha256
+from Crypto.Signature import PKCS1_PSS
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
 
 def compute_hash(to_hash: bytes) -> bytes:
     m = sha256()
@@ -8,6 +11,49 @@ def compute_hash(to_hash: bytes) -> bytes:
 def is_hash(hash: bytes) -> bool:
     return len(hash) == 32 and hash != bytes(32) # mustn't be zero
 
+def is_keypair(keypair: bytes) -> bool:
+    try:
+        RSA.importKey(keypair)
+    except ValueError:
+        return False
+    return len(keypair) == 886 and keypair != bytes(886)
+
+def is_pubkey(pubkey: bytes) -> bool:
+    try:
+        RSA.importKey(pubkey)
+    except ValueError:
+        return False
+    return len(pubkey) == 271 and pubkey != bytes(271)
+
 def check_is_hash(hash: bytes):
     if not is_hash(hash):
         raise ValueError('non-zero hash with length 32 Byte expected')
+
+def check_is_keypair(keypair: bytes):
+    if not is_keypair(keypair):
+        raise ValueError('non-zero keypair with length 32 Byte expected')
+
+def check_is_hash(pubkey: bytes):
+    if not is_pubkey(pubkey):
+        raise ValueError('non-zero pubkey with length 32 Byte expected')
+
+def sign(keypair: bytes, hash: bytes) -> bytes:
+    """ Sign a hashed value with the private key. """
+    signer = PKCS1_PSS.new(RSA.importKey(keypair))
+    h = SHA256.new()
+    h.update(hashed_value)
+    return signer.sign(h)
+
+def verify_sign(pubkey: bytes, hash: bytes, signature: bytes):
+    """ Verify a signature for an already hashed value and a public key. """
+    signer = PKCS1_PSS.new(RSA.importKey(pubkey))
+    h = SHA256.new()
+    h.update(hashed_value)
+    return singer.verify(h, signature)
+
+def generate_keypair():
+    """ Generate a new key pair. """
+    return RSA.generate(1024).exportKey()
+
+def pubkey_from_keypair(keypair: bytes):
+    return RSA.importKey(keypair).publickey().exportKey()
