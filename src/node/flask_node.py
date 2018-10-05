6e944269c9f5d5bd52eb3b4eb36faba2cb02ad1d
haslersn <sebastian.hasler@gmx.net>
2018-10-05 12:58:16 +0200

    node/FlaskNode: Simplify current API

--- src/node/flask_node.py
+++ src/node/flask_node.py
@@ -8,6 +8,7 @@ from src.blockchain.new_block import *
 
 ########
 rest_port = 5000
+logging.basicConfig(level=logging.INFO)
 ########
 
 class FlaskNode:
@@ -19,12 +20,23 @@ class FlaskNode:
 node = FlaskNode(rest_port)
 app = node._app
 
+def _get_block_by_hash(hash: bytes):
+    logging.info('Block requested with hash {}'.format(hash))
+    block = Block.get_from_hash(hash)
+    if block is None:
+        logging.info('Requested block is unknown')
+        return ''
+    result = {}
+    result['block'] = block.to_json_compatible()
+    result['transactions'] = [ tx.to_json_compatible() for tx in block.skeleton.transactions ]
+    return json.dumps(result)
+
 @app.route("/get_latest_block", methods=['POST'])
 def get_latest_block():
     """
     Returns the known block with the highest accumulated difficulty
     """
-    return '' if node._latest_block is None else json.dumps(node._latest_block.to_json_compatible())
+    return '' if node._latest_block is None else _get_block_by_hash(node._latest_block.hash)
 
 @app.route("/get_block_by_hash", methods=['POST'])
 def get_block_by_hash():
@@ -32,37 +44,21 @@ def get_block_by_hash():
     Returns the block with the requested hash if it is known.
     """
     hash = unhexlify(flask.request.json['hash'])
-    return '' if hash not in node._blocks else json.dumps(node._blocks[hash].to_json_compatible())
-
-@app.route("/get_block_transactions", methods=['POST'])
-def get_block_transactions():
-    """
-    Returns a list of transactions that are contained in the block with the requested hash.
-    """
-
-    hash = unhexlify(flask.request.json['hash'])
-    block = Block.get_from_hash(hash)
-    print(hash)
-    if block is None:
-        return ''
-    print('block was found')
-    print(block)
-    transactions = node._blocks[hash].transactions
-    print(transactions)
-    var = {}
-    var['transactions'] = [ tx.to_json_compatible() for tx in transactions ]
-    print(var)
-    return json.dumps(var)
+    return _get_block_by_hash(hash)
 
 @app.route("/add_block", methods=['POST'])
 def add_block():
     """
     Appends a block to the blockchain.
     """
-    block_compat = flask.request.json['block']
+    logging.info('Adding a block requested')
     transactions_compat = flask.request.json['transactions']
-    transactions = [ Transaction.from_json_compatible(tx_compat) for tx_compat in transactions_compat ]
+    logging.info('Transactions: {}'.format(transactions_compat))
+    transactions = map(Transaction.from_json_compatible, transactions_compat)
+    block_compat = flask.request.json['block']
+    logging.info('Block: {}'.format(block_compat))
     block = Block.from_json_compatible(block_compat, transactions)
-    if node._latest_block is None or block.skeleton.accumulated_difficulty > node._latest_block.skeleton.accumulated_difficulty:
+    target = 0 if node._latest_block is None else node._latest_block.skeleton.accumulated_difficulty
+    if block.skeleton.accumulated_difficulty > target:
         node._latest_block = block
     return json.dumps(True)
